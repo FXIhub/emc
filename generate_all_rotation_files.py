e2f7ee59ad1a204ed7f8d3028af72d22d21c5cf4
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2013-08-20 11:34:46 +0200

    Added a few files that should already have been included.

---
+++ generate_all_rotation_files.py
@@ -0,0 +1,15 @@
+import os
+from optparse import OptionParser
+
+default_executable = '/home/ekeberg/Work/programs/emc/git/emc/build/generate_rotations'
+
+parser = OptionParser(usage="%prog MAX_N -e EXECUTABLE")
+parser.add_option('-e', action="store", type="string", dest="executable", default=default_executable, help="Path to the generate_rotations.")
+options, args = parser.parse_args()
+
+n = int(args[0])
+
+for i in range(n+1):
+    command = '%s %d' % (options.executable, i)
+    print command
+    os.system(command)
